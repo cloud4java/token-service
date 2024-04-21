95d028198c5aa564384f4e8191dcbae7c380f9d5
Dorival Querino da Silva <dorival.querino@gmail.com>
2024-04-21 01:34:12 -0300

    first commit

---
+++ src/main/java/com/silvasoft/tokenservice/web/input/Token.java
@@ -0,0 +1,13 @@
+package com.silvasoft.tokenservice.web.input;
+
+public class Token {
+    private String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(final String value) {
+        this.value = value;
+    }
+}
